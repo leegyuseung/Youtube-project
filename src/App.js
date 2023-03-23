d02a80af6ce997b4b9567ff33de7c82996cf1120
gyuseung <lgs4002@naver.com>
2023-03-23 22:33:12 +0900

    [feat] 라우터 설정하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Detail from "./components/Detail";
+import Videos from "./components/Videos";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Videos />,
+    children: [
+      {
+        path: "/videos",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/query",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/watch/id",
+        element: <Detail />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
