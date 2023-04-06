f40e1cfe597f23b0d3b68efb91dc2acb50f57fa1
gyuseung <lgs4002@naver.com>
2023-04-06 23:26:11 +0900

    [feat] 상세페이지 스타일링

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import { formatAgo } from "../utils/date";
 import { useNavigate } from "react-router-dom";
 
-export default function VideoCard({ video }) {
+export default function VideoCard({ video, type }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
   const navigate = useNavigate();
+  const isList = type === "list";
   return (
     <li
+      className={isList ? "flex gap-1 m-2" : ""}
       onClick={() => {
         // 두번 째 인자로 데이터를 보내준다.
         navigate(`/videos/watch/${video.id}`, { state: { video } });
       }}
     >
-      <img className="w-full" src={thumbnails.medium.url} alt={title} />
+      <img
+        className={isList ? "w-60 mr-2" : "w-full"}
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
         <p className="text-sm opacity-80">{channelTitle}</p>
